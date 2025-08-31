baddbed51b50dbd7e419f75d7ffdbf41836751f2
yoshi <mdmdmdmdmd57@gmail.com>
2025-08-31 19:19:15 +0900

    key名がtimeの方が被りやすいため変更

--- sample-vite/src/components/display/DisplayStydyResultList.jsx
+++ sample-vite/src/components/display/DisplayStydyResultList.jsx
@@ -2,7 +2,7 @@ export const DisplayStydyResultList = ({ records }) => {
   return (
     <>
       {records.map((data) => (
-        <p key={data.time}>{`${data.title} ${data.time}時間`} </p>
+        <p key={data.title}>{`${data.title} ${data.time}時間`} </p>
       ))}
     </>
   );
